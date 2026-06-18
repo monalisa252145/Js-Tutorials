f88535ca25b7cc09a396bdf4d3836d2232b8e998
Monalisa <152963081+monalisa252145@users.noreply.github.com>
2026-06-18 18:55:35 +0000

    datatypes

---
+++ 01-basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 144553
+let accountEmail = "monalisa@google.com"
+var accountPassword = "12345"
+accountCity  = "Jaipur"
+let accountState;
+
+//accountId = 2
+
+accountEmail = "hc@hc.com"
+accountPassword = "21212121"
+accountCity = "Delhi"
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
---
+++ 01-basics/02_dataTypes.js
@@ -0,0 +1,25 @@
+"use strict";  //treat all JS code as newer version
+
+// alert(3 + 3) // we are using nodejs, not browser
+
+let name = "Mona"
+let age = 18
+let isLoggedIn = false
+
+
+//number => 2 to power 53
+// bigint
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefined =>
+// symbol
+
+
+//object
+
+console.log(typeof "hitesh");
+console.log(typeof "age");
+console.log(typeof age);
+console.log(typeof null);
+console.log(typeof undefined);
